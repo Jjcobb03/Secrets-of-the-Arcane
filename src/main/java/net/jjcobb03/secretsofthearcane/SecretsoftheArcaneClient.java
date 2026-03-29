5cfafbbe3c877103ade9e03b536f26b785cc8280
Jjcobb03 <38336294+Jjcobb03@users.noreply.github.com>
2026-03-28 23:00:43 -0400

    Updated Information
    Updated the information in gradle.properties, and replaced some of the example names with more fitting names.

--- src/main/java/net/jjcobb03/secretsofthearcane/SecretsoftheArcaneClient.java
+++ src/main/java/net/jjcobb03/secretsofthearcane/SecretsoftheArcaneClient.java
@@ -1,4 +1,4 @@
-package com.example.secretsofthearcane;
+package net.jjcobb03.secretsofthearcane;
 
 import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
@@ -11,9 +11,9 @@ import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 // This class will not load on dedicated servers. Accessing client side code from here is safe.
-@Mod(value = SecretsoftheArcane.MODID, dist = Dist.CLIENT)
+@Mod(value = SecretsOfTheArcane.MOD_ID, dist = Dist.CLIENT)
 // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-@EventBusSubscriber(modid = SecretsoftheArcane.MODID, value = Dist.CLIENT)
+@EventBusSubscriber(modid = SecretsOfTheArcane.MOD_ID, value = Dist.CLIENT)
 public class SecretsoftheArcaneClient {
     public SecretsoftheArcaneClient(ModContainer container) {
         // Allows NeoForge to create a config screen for this mod's configs.
@@ -25,7 +25,7 @@ public class SecretsoftheArcaneClient {
     @SubscribeEvent
     static void onClientSetup(FMLClientSetupEvent event) {
         // Some client setup code
-        SecretsoftheArcane.LOGGER.info("HELLO FROM CLIENT SETUP");
-        SecretsoftheArcane.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+        SecretsOfTheArcane.LOGGER.info("HELLO FROM CLIENT SETUP");
+        SecretsOfTheArcane.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
     }
 }
