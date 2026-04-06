a9c8286a79134ce3533799e69a778e4116c775c0
Jjcobb03 <38336294+Jjcobb03@users.noreply.github.com>
2026-04-06 14:24:35 -0400

    Added Block Class
    Setup the block class, and included an example block.

--- src/main/java/net/jjcobb03/secretsofthearcane/SecretsOfTheArcane.java
+++ src/main/java/net/jjcobb03/secretsofthearcane/SecretsOfTheArcane.java
@@ -1,22 +1,12 @@
 package net.jjcobb03.secretsofthearcane;
 
+import net.jjcobb03.secretsofthearcane.block.ModBlocks;
 import net.jjcobb03.secretsofthearcane.item.ModItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
 
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
@@ -26,10 +16,6 @@ import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
-import net.neoforged.neoforge.registries.DeferredBlock;
-import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredItem;
-import net.neoforged.neoforge.registries.DeferredRegister;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(SecretsOfTheArcane.MOD_ID)
@@ -89,6 +75,8 @@ public class SecretsOfTheArcane {
 
         // Call the Deferred Register for Items from ModItems
         ModItems.register(modEventBus);
+        // Call the Deferred Register for Blocks from ModBlocks
+        ModBlocks.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
@@ -121,6 +109,10 @@ public class SecretsOfTheArcane {
         // Add the test item to the Ingredients creative tab
         if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
             event.accept(ModItems.TEST_ITEM);
+        }
+
+        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
+            event.accept(ModBlocks.ROWANLOG);
         }
     }
 
---
+++ src/main/java/net/jjcobb03/secretsofthearcane/block/ModBlocks.java
@@ -0,0 +1,43 @@
+package net.jjcobb03.secretsofthearcane.block;
+
+import net.jjcobb03.secretsofthearcane.SecretsOfTheArcane;
+import net.jjcobb03.secretsofthearcane.item.ModItems;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.function.Supplier;
+
+public class ModBlocks {
+    // Create a Deferred Register to hold Blocks which will all be registered under the "secretsofthearcane" namespace
+    public static final DeferredRegister.Blocks BLOCKS =
+            DeferredRegister.createBlocks(SecretsOfTheArcane.MOD_ID);
+
+    // Custom blocks to register
+    public static final DeferredBlock<Block> ROWANLOG = registerBlock("rowan_log",
+            () -> new Block(BlockBehaviour.Properties.of().strength(1f).sound(SoundType.WOOD)));
+
+    // Registers the Block and the BlockItem
+    private static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block) {
+        DeferredBlock<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    // Helper method that creates the BlockItem
+    private static <T extends Block> void registerBlockItem(String name, DeferredBlock<T> block) {
+        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    }
+
+    // Register the Deferred Register to the mod event bus so blocks get registered
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+
+    }
+
+}
