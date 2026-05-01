8a79ad8a87640fa9352c9a933d3f4c929a12e3b1
Jjcobb03 <38336294+Jjcobb03@users.noreply.github.com>
2026-05-01 11:43:54 -0400

    Updated Block Datagen
    Block datagen is now able to handle a RotatedPillarBlock. As such, I have also removed the .json files associated with the Rowan Log from src/main/resources/assets

--- src/main/java/net/jjcobb03/secretsofthearcane/datagen/ModBlockStateProvider.java
+++ src/main/java/net/jjcobb03/secretsofthearcane/datagen/ModBlockStateProvider.java
@@ -2,7 +2,9 @@ package net.jjcobb03.secretsofthearcane.datagen;
 
 import net.jjcobb03.secretsofthearcane.SecretsOfTheArcane;
 import net.jjcobb03.secretsofthearcane.block.ModBlocks;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.data.PackOutput;
+import net.minecraft.world.level.block.RotatedPillarBlock;
 import net.neoforged.neoforge.client.model.generators.BlockStateProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.neoforge.registries.DeferredBlock;
@@ -24,6 +26,14 @@ public class ModBlockStateProvider extends BlockStateProvider {
         blockWithItem(ModBlocks.ROWAN_PLANKS);
         blockWithItem(ModBlocks.TERRA_ORE);
         blockWithItem(ModBlocks.TERRA_DEEPSLATE_ORE);
+
+        // Pillar Blocks
+        rotatedPillarBlockWithItem((RotatedPillarBlock) ModBlocks.ROWAN_LOG.get());
+//        logBlock((RotatedPillarBlock) ModBlocks.ROWAN_LOG.get());
+//        simpleBlockItem(
+//                ModBlocks.ROWAN_LOG.get(),
+//                models().getExistingFile(modLoc("block/rowan_log"))
+//        );
     }
 
     /**
@@ -34,4 +44,19 @@ public class ModBlockStateProvider extends BlockStateProvider {
     private void blockWithItem(DeferredBlock<?> deferredBlock) {
         simpleBlockWithItem(deferredBlock.get(), cubeAll(deferredBlock.get()));
     }
+
+    /**
+     * Helper method for RotatedPillarBlocks that generates blockstates, block model, and item model.
+     *
+     * @param block - A RotatedPillarBlock that will be created
+     */
+    private void rotatedPillarBlockWithItem(RotatedPillarBlock block) {
+        String name = BuiltInRegistries.BLOCK.getKey(block).getPath();
+
+        // Blockstates and Block Model
+        logBlock(block);
+        // Item Model
+        simpleBlockItem(block, models().getExistingFile(modLoc("block/" + name)));
+    }
+    
 }
